fd13668e07c8adeebe691106e90b30ba76704be6
kovad003 <kovacsdansan@gmail.com>
2022-12-30 22:04:59 +0200

    Walking skeleton has been created
    - Set up developer environment
    - Installed most important extensions/Nuget packages:
      > Entity Framework
      > SQLite
    - Created .Net projects
    - Creating and seeding basic DB
    - Added API controller
    - Tested APIs using Postman

---
+++ API/WeatherForecast.cs
@@ -0,0 +1,19 @@
+namespace API;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    
+    public string Summary { get; set; } 
+    /* @Dan:
+    public string? Summary { get; set; } -> remove "?"
+    *.csproj -> nullable = disable
+    Strings are now optional.
+    */
+    
+
+}
---
+++ Domain/Activity.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain;
+
+// Names must be short and simple -> EF will create the DB tables based pn 'em
+public class Activity
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string City { get; set; }
+    public string Venue { get; set; }
+}
