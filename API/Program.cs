e1decd83cca34fab7d79c441043aeea46dddf827
kovad003 <kovacsdansan@gmail.com>
2023-01-07 00:58:04 +0200

    Implemented Identity, Auth, Login & Reg
    - Added user entity
    - Added IdentityDbContext
    - Seeding users into the db
    - Creating user DTOs
    - Adding account controller
    - Added JSON Web Tokens
    - Creating a token service
    - Authenticating the app
    - Creating auth policy
    - Implemented registration and login services with validation

--- API/Program.cs
+++ API/Program.cs
@@ -3,14 +3,25 @@
 
 using API.Extensions;
 using API.MiddleWare;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    // Every single API endpoint will require authentication.
+    // The same as adding the [Authorize] annotation to each EP.
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -25,6 +36,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("CorsPolicy");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -36,8 +48,9 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception e)
 {
